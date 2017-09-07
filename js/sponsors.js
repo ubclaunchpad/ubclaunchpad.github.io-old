760bfc7558f322958eca3a740801f3f712fba663
Jordan Schalm <jordan.schalm@gmail.com>
2017-09-06 20:43:50 -0700

    add LHL logo to sponsors section

--- js/sponsors.js
+++ js/sponsors.js
@@ -2,17 +2,20 @@
 // logo is moused over
 
 (function () {
+	// NOTE: Removing this for now because LHL logo doesn't have a
+	// transparent background. Can revisit later.
+	return;
 	var section = document.getElementById('sponsors');
 	var cl = section.classList;
 
 	var github = document.getElementById('github');
 	var digitalOcean = document.getElementById('digital-ocean');
 	var mda = document.getElementById('mda');
+	var lighthouseLabs = document.getElementById('lighthouse-labs');
 
 	github.addEventListener('mouseover', function () {
 		cl.add('github');
 	});
-
 	github.addEventListener('mouseout', function () {
 		cl.remove('github');
 	});
@@ -20,7 +23,6 @@
 	digitalOcean.addEventListener('mouseover', function () {
 		cl.add('digital-ocean');
 	});
-
 	digitalOcean.addEventListener('mouseout', function () {
 		cl.remove('digital-ocean');
 	});
@@ -28,8 +30,14 @@
 	mda.addEventListener('mouseover', function () {
 		cl.add('mda');
 	});
-
 	mda.addEventListener('mouseout', function () {
 		cl.remove('mda');
+	});
+
+	lighthouseLabs.addEventListener('mouseover', function () {
+		cl.add('lighthouse-labs');
+	});
+	lighthouseLabs.addEventListener('mouseout', function () {
+		cl.remove('lighthouse-labs');
 	});
 }())
